c2e7c44820262eb37316485e31de67c6ae2934bf
Zachary Servies <zachservies@gmail.com>
2020-09-17 23:25:58 -0400

    Added ability to pass score data between components.

--- golfriend-ui/src/App.js
+++ golfriend-ui/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./App.css";
-import ScoreInput from './components/scoreInput';
+import ScoreInput from './components/ScoreInput';
 
 function App() {
+
+  const createScore = (data) => {
+    console.log(data);
+  }
+
   return (
     <div className="App">
       <div className="App-header">
-        <ScoreInput></ScoreInput>
+        <ScoreInput createScore={createScore}></ScoreInput>
       </div>
     </div>
   );
--- golfriend-ui/src/components/scoreInput.js
+++ golfriend-ui/src/components/scoreInput.js
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import SaveIcon from "@material-ui/icons/Save";
 
-export default function scoreInput(props) {
+export default function ScoreInput(props) {
+  const [courseName, setCourseName] = useState("");
+  const [score, setScore] = useState("");
+  const [coursePAR, setCoursePAR] = useState("");
+
+  const submitScore = (event) => {
+    event.preventDefault();
+    let scoreSubmission = {
+      courseName,
+      score,
+      coursePAR,
+    };
+    props.createScore(scoreSubmission);
+  };
   return (
     <div>
-      <form noValidate autoComplete="off">
-        <TextField id="outlined-basic" label="Course name" variant="outlined" />
-        <TextField id="outlined-basic" label="Score" variant="outlined" />
-        <TextField id="outlined-basic" label="Course rating" variant="outlined" />
-        <TextField id="outlined-basic" label="Course slope" variant="outlined" />
-        <TextField id="outlined-basic" label="Course PAR" variant="outlined" />
+      <form noValidate autoComplete="off" onSubmit={submitScore}>
+        <TextField
+          onChange={(event) => setCourseName(event.target.value)}
+          id="outlined-basic"
+          label="Course name"
+          variant="outlined"
+        />
+        <TextField
+          onChange={(event) => setScore(event.target.value)}
+          id="outlined-basic"
+          label="Score"
+          variant="outlined"
+        />
+        <TextField
+          onChange={(event) => setCoursePAR(event.target.value)}
+          id="outlined-basic"
+          label="Course PAR"
+          variant="outlined"
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          startIcon={<SaveIcon />}
+          type="submit"
+        >
+          Save
+        </Button>
       </form>
     </div>
   );
