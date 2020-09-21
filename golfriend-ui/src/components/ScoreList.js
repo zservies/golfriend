8dfed8cf50e644a82f6745a2c3002d9829d8890f
Zachary Servies <zachservies@gmail.com>
2020-09-20 23:01:21 -0400

    Changed color of table cells to white.

--- golfriend-ui/src/components/ScoreList.js
+++ golfriend-ui/src/components/ScoreList.js
@@ -5,28 +5,69 @@ import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  tableCell: {
+    color: "white",
+    fontSize: "1rem",
+  },
+  tableHead: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: "1rem",
+  },
+  deleteIcon: {
+    color: "grey",
+  },
+});
 
 export default function ScoreList(props) {
+  const classes = useStyles();
+
+  const deleteScore = (event) => {
+    console.log(event.target);
+  };
+
   return (
     <div>
       <TableContainer>
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>Course</TableCell>
-              <TableCell>Score</TableCell>
-              <TableCell>PAR</TableCell>
-              <TableCell>Over/Under</TableCell>
+              <TableCell className={classes.tableHead}>Course</TableCell>
+              <TableCell className={classes.tableHead}>Score</TableCell>
+              <TableCell className={classes.tableHead}>PAR</TableCell>
+              <TableCell className={classes.tableHead}>Over/Under</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {props.scores &&
               props.scores.map((scoreItem) => (
                 <TableRow key={scoreItem.id}>
-                  <TableCell>{scoreItem.course}</TableCell>
-                  <TableCell>{scoreItem.score}</TableCell>
-                  <TableCell>{scoreItem.coursePar}</TableCell>
-                  <TableCell>{scoreItem.scoreToPar}</TableCell>
+                  <TableCell className={classes.tableCell}>
+                    {scoreItem.course}
+                  </TableCell>
+                  <TableCell className={classes.tableCell}>
+                    {scoreItem.score}
+                  </TableCell>
+                  <TableCell className={classes.tableCell}>
+                    {scoreItem.coursePar}
+                  </TableCell>
+                  <TableCell className={classes.tableCell}>
+                    {scoreItem.scoreToPar}
+                  </TableCell>
+                  <TableCell>
+                    <IconButton
+                      className={classes.deleteIcon}
+                      onClick={deleteScore}
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  </TableCell>
                 </TableRow>
               ))}
           </TableBody>
