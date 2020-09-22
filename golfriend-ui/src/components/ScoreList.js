51193952359bbee9643fed782952b5a715610a42
Zachary Servies <zachservies@gmail.com>
2020-09-21 23:39:06 -0400

    Removed unused import.

--- golfriend-ui/src/components/ScoreList.js
+++ golfriend-ui/src/components/ScoreList.js
@@ -5,7 +5,6 @@ import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
